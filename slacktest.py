90089d0d593b9e742e6daeabbe47fe16973c0bb2
Defalt <thedefalt@protonmail.com>
2017-01-23 14:46:07 -0600

    Update slacktest.py

--- slacktest.py
+++ slacktest.py
@@ -3,7 +3,7 @@ from Legobot.Lego import Lego
 from Legobot.Connectors.Slack import Slack
 from Local import Defalt, Scan, test
 
-SLACK_TOKEN = 'xoxp-130615944049-131372502388-131447952709-8ef50f341b0c3abc6f98e29cc5d2ed3f'
+SLACK_TOKEN = 'DOTNWORRYAS;LDKFJ'xoxp-130615944049-131372502388-131447952709-8ef50f341b0c3abc6f98e29cc5d2ed3f
 
 # As usual, all the Pykka stuff to get us initialized
 lock = threading.Lock()
